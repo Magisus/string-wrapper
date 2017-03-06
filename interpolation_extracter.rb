e7c2fc0f258841ac3de977dd48f96ae36b9baa2b
Maggie Dreyer <maggie@puppet.com>
2017-03-06 15:17:36 -0800

    Create a hash of interpolated values

--- interpolation_extracter.rb
+++ interpolation_extracter.rb
@@ -1,17 +1,37 @@
 require 'pry'
 require 'pry-nav'
 
-class StringWrapper < Parser::Rewriter
+class InterpolationExtracter < Parser::Rewriter
   def on_send(node)
-    method_name = node.loc.selector.source
-    return unless method_name != "_"
+    receiver_node, method_name, *arg_nodes = *node
+    if method_name == :_
+      process_string(arg_nodes[0])
+    end
     super
   end
 
-  def on_dstr(node)
+  def process_string(node)
+    # Strings with interpolation are dstr nodes
+    return if node.type != :dstr
+
+    interpolated_values = {}
+    count = 0
     node.children.each do |child|
-      puts child
+      if child.type == :begin
+        value = child.children[0]
+        hash_key = "value"
+        if value.type == :lvar
+          hash_key = value.loc.name.source
+        else
+          # These are placeholders that will manually need to be given
+          # a descriptive name
+          hash_key << "#{count}"
+          count += 1
+        end
+        interpolated_values[hash_key] = value
+      end
     end
+    puts interpolated_values
   end
 end
 
