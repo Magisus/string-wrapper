2c213e2f796296c8083a452e9cd92f069defb6ed
Maggie Dreyer <maggie@puppet.com>
2017-03-07 09:35:57 -0800

    Replace interpolated values with format string

--- interpolation_extracter.rb
+++ interpolation_extracter.rb
@@ -5,7 +5,7 @@ class InterpolationExtracter < Parser::Rewriter
   def on_send(node)
     receiver_node, method_name, *arg_nodes = *node
     if method_name == :_
-      process_string(arg_nodes[0])
+      format_hash = process_string(arg_nodes[0])
     end
     super
   end
@@ -17,10 +17,13 @@ class InterpolationExtracter < Parser::Rewriter
     interpolated_values = {}
     count = 0
     node.children.each do |child|
+      # dstrs are split into "str" segments and other segments.
+      # The "other" segments are the interpolated values.
       if child.type == :begin
         value = child.children[0]
         hash_key = "value"
         if value.type == :lvar
+          # Use the variable's name as the format key
           hash_key = value.loc.name.source
         else
           # These are placeholders that will manually need to be given
@@ -29,9 +32,12 @@ class InterpolationExtracter < Parser::Rewriter
           count += 1
         end
         interpolated_values[hash_key] = value
+
+        # Replace interpolation with format string
+        replace(child.loc.expression, "%{#{hash_key}}")
       end
     end
-    puts interpolated_values
+    return interpolated_values
   end
 end
 
